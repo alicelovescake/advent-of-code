a12742f6b1b5d94fdc70417647681249b139b5b4
Alice Zhao <azhao991@gmail.com>
2022-11-30 21:38:48 -0800

    day 1 solutions

--- solutions/2022/01/index.js
+++ solutions/2022/01/index.js
@@ -1,19 +1,28 @@
+import { example, data } from "./input";
 
-import { example, data } from './input'
+export const inputParser = (input) =>
+  input.split("\n\n").map((set) => set.split("\n"));
 
-export const inputParser = (input) => input.split('\n')
+const parsedData = inputParser(data);
 
-const parsedData = inputParser(input)
+const getTotal = (input) => {
+  return input.map((set) =>
+    set.reduce((acc, calories) => parseInt(calories) + acc, 0)
+  );
+};
 
 export const part1 = (input = parsedData) => {
-  return
-}
+  return Math.max(...getTotal(input));
+};
 
 export const part2 = (input = parsedData) => {
-  return
-}
+  const total = getTotal(input);
+  total.sort((a, b) => b - a);
+
+  return total[0] + total[1] + total[2];
+};
 
 export default {
   part1,
   part2,
-}
+};
